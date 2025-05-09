7105ab3520139a4a56db5d842874b475d3844bf9
aijia1314-yok <2060572551@qq.com>
2025-05-09 14:20:14 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oBF-67TPHmKuGV5OD6yLiK9zzUWo',
+      id: 'oBF-67RJwd_CVGmNRKxpcTemrxms',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'HnoDto5inZ8JpzRwlbM5i_WBC4u_EAlQNC8gmKUxxbs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
